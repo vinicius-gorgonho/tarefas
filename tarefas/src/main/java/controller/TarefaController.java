b28d02d992712d05855b9d75a5735bbc1d4c0bb8
Vinícius Gorgonho <vinicius2pi@gmail.com>
2024-05-23 09:00:06 -0400

    Testando a configuração do JSF

---
+++ tarefas/src/main/java/controller/TarefaController.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import java.io.Serializable;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+
+@Named
+@ViewScoped
+public class TarefaController implements Serializable{
+    
+    String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String adicionar(){
+        return "";
+    }
+    
+    
+}
